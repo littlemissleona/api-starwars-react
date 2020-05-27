7d742e771ac945e59e96293b69cb29dfd3c32caf
littlemissleona <leonawyso@gmail.com>
2020-05-27 16:35:45 +0100

    Updates

---
+++ precache-manifest.11745397fa6fb9713db6b3abeb2e9be6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaea1b7bb53f3574a89c616dae057ffc",
+    "url": "/api-starwars-react/index.html"
+  },
+  {
+    "revision": "f0c17600a2d038ad2b3b",
+    "url": "/api-starwars-react/static/css/main.4758403c.chunk.css"
+  },
+  {
+    "revision": "84e2c0761ccc4a76d708",
+    "url": "/api-starwars-react/static/js/2.7d5f804b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/api-starwars-react/static/js/2.7d5f804b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0c17600a2d038ad2b3b",
+    "url": "/api-starwars-react/static/js/main.d98ebda9.chunk.js"
+  },
+  {
+    "revision": "4e409e9ae80da9bc9f1c",
+    "url": "/api-starwars-react/static/js/runtime-main.12204df0.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/api-starwars-react/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "9c91297e17268fbd21d698ca6e526faa",
+    "url": "/api-starwars-react/static/media/Starjhol.9c91297e.ttf"
+  }
+]);
